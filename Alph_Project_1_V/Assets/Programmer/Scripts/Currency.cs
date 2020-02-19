f04d31e4a2ab019ce6da64446459e6dd4912f09f
SumeetDevkai <47910905+SumeetDevkai@users.noreply.github.com>
2020-02-19 10:49:21 +0530

    spit mechanic

---
+++ Alph_Project_1_V/Assets/Programmer/Scripts/Currency.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Currency : MonoBehaviour
+{
+    public static Currency instance;
+
+    public Text currency1, currency2;
+
+    public int mineralCurr1 = 0, mineralCurr2 = 0;
+    void Awake()
+    {
+        instance = this;
+    }
+    void Update()
+    {
+        currency1.text = mineralCurr1.ToString("Mineral 1 : ");
+        currency2.text = mineralCurr2.ToString("Mineral 2 : ");
+    }
+}
---
+++ Alph_Project_1_V/Assets/Programmer/Scripts/Mineral2.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mineral2 : MonoBehaviour
+{   
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
