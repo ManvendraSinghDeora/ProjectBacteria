805502d54f2db16966efa32913551f354d69d018
kshitizvyas <42069+kshitizvyas@users.noreply.github.com>
2020-03-02 01:21:46 +0530

    level design

---
+++ Alph_Project_1_V/Assets/boss files/scripts/patrol.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class patrol : MonoBehaviour
+{
+    public float speed,startwaittime;
+    float waittime;
+
+    public Transform[] movespot;
+    int randomspot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        waittime = startwaittime;
+        randomspot = Random.Range(0, movespot.Length);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, movespot[randomspot].position, 
+            speed * Time.deltaTime);
+
+        if (Vector2.Distance(transform.position, movespot[randomspot].position) < 0.1f)
+        {
+            if(waittime <= 0)
+            {
+                randomspot = Random.Range(0, movespot.Length);
+                waittime = startwaittime;
+            }
+            else
+            {
+                waittime -= Time.deltaTime;
+            }
+        } 
+    }
+}
